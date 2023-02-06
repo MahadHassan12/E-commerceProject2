bf64c0d64c4c3740548f360b4b408f8300bec87e
John Doe <johndoe@example.com>
2023-02-06 13:10:36 +0100

    first

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    height: 30px;
+    background-color: #64cdee;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 14px;
+    font-weight: 500;
+`
+
+const Announcement = () => {
+  return (
+    <Container>
+        Super Deal! Fri frakt vid beställningar över 1000 kr
+    </Container>
+  )
+}
+
+export default Announcement
+
