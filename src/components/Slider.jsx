3c2ab6cd133de0171ccee9b74f9908ddf65be95b
John Doe <johndoe@example.com>
2023-02-09 13:33:06 +0100

    template

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,5 +1,7 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons"
+import { useState } from "react";
 import styled from "styled-components"
+import { sliderItems } from "../data";
 
 
 const Container = styled.div`
@@ -7,6 +9,7 @@ const Container = styled.div`
     height: 100vh;
     display: flex;
     position: relative;
+    overflow: hidden;
 `;
 const Arrow = styled.div`
     width: 50px;
@@ -19,14 +22,18 @@ const Arrow = styled.div`
     position: absolute;
     top: 0;
     bottom: 0;
-    left: ${props=> props.direction === 'left' && '10px'};
+    left:  ${props=> props.direction === 'left' && '10px'};
     right: ${props=> props.direction === 'right' && '10px'};
     margin: auto;
     cursor: pointer;
     opacity: 0.5;
+    z-index: 2;
 `;
 const Wrapper = styled.div`
     height: 100%;
+    display: flex;
+    transition: all 1.5s ease;
+    transform: translateX(${props=>props.slideIndex * -100}vw);
 `;
 
 const Slide = styled.div`
@@ -34,6 +41,7 @@ const Slide = styled.div`
     height: 100vh;
     display: flex;
     align-items: center;
+    background-color: #${props=> props.bg};
 `;
 const ImgContainer = styled.div`
     height: 100%;
@@ -65,24 +73,34 @@ const Button = styled.button`
 `
 
 const Slider = () => {
+    const [slideIndex, setSlideIndex] = useState(0);
+    const handleClick = (direction) => {    
+        if(direction==='left'){
+            setSlideIndex(slideIndex > 0 ? slideIndex-1 : 2)
+        } else {
+            setSlideIndex(slideIndex< 2 ? slideIndex + 1 : 0)
+        }
+    }
   return (
     <Container>
-        <Arrow direction='left'>
+        <Arrow direction='left' onClick={()=> handleClick('left')}>
             <ArrowLeftOutlined />
         </Arrow>
-        <Wrapper>
-            <Slide>
-                <ImgContainer>
-                    <Image src='https://www.hp.com/se-sv/shop/Html/Merch/Images/c08262515_1750x1285.jpg?imwidth=750' />
-                </ImgContainer>
-                <InfoContainer>
-                    <Title>Vinter deals!!</Title>
-                    <Desc>Stationära Datorer 20% rabatt</Desc>
-                    <Button>KÖP NU</Button>
-                </InfoContainer>
-            </Slide>
+        <Wrapper slideIndex={slideIndex}>
+            {sliderItems.map(item=>(
+                <Slide bg={item.bg}>
+                    <ImgContainer>
+                        <Image src={item.img} />
+                    </ImgContainer>
+                    <InfoContainer>
+                        <Title>{item.title}</Title>
+                        <Desc>{item.desc}</Desc>
+                        <Button>Visa Mer!</Button>
+                    </InfoContainer>
+                </Slide>
+            ))}
         </Wrapper>
-        <Arrow direction='right'>
+        <Arrow direction='right' onClick={()=> handleClick('right')}>
             <ArrowRightOutlined />
         </Arrow>
     </Container>
---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const sliderItems = [
+    {
+        id:1,
+        img: 'https://www.hp.com/se-sv/shop/Html/Merch/Images/c08262515_1750x1285.jpg?imwidth=750',
+        title: 'Vinter deals!',
+        desc: 'Stationära Datorer 20% rabatt på alla köp tills mars månad.',
+        bg: 'E0FFFF'
+    },
+
+    {
+        id:2,
+        img: 'https://thumbs.dreamstime.com/b/laptop-computer-blank-white-screen-mobile-table-cafe-background-139812612.jpg',
+        title: 'Populära köp! Bärbara datorer',
+        desc: 'Billiga datorer för endast 5000 kr.',
+        bg: '87CEFA'
+    },
+    {
+        id:3,
+        img: 'https://i.insider.com/626ff8180983640018c0abf4?width=1136&format=jpeg',
+        title: 'Veckans  deals!',
+        desc: 'Video spel! 10 % rabatt på alla konsoler. Köp en konsol och få med 2 videospel på köpet.',
+        bg: '00BFFF'
+    }
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: 'https://www.komplett.se/img/p/1024/1213652.jpg',
+        title: 'Stationära datorer'
+    },
+    {
+        id: 2,
+        img: 'https://www.komplett.se/img/p/300/1196978.jpg',
+        title: 'Bärbara datorer'
+    },
+    {
+        id: 3,
+        img: 'https://www.komplett.se/img/p/900/1217813.jpg',
+        title: 'Mobiltelefoner'
+    },
+
+]
